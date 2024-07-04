b482d654d071d8c6393595a729a15eb89e28fd22
iraisaeva2001 <ira.isaeva2001@mail.ru>
2024-07-04 14:45:07 +0300

    Update dag_test.py

--- dags/dag_test.py
+++ dags/dag_test.py
@@ -1,8 +1,7 @@
 import datetime
 from airflow import DAG
-from airflow.hooks.base import BaseHook
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 
 # Определение параметров DAG
@@ -11,22 +10,22 @@ dag = DAG(
     description='test_dag',
     schedule_interval=None,
     start_date=datetime.datetime(2024, 7, 2),
-    catchup=False,
+    catchup=False
 )
 
 # Задача DummyOperator, которая служит начальной точкой
-start_step = DummyOperator(task_id="start_step", dag=dag)
+start_step = EmptyOperator(task_id="start_step", dag=dag)
 
 # Задача PostgresOperator для выполнения SQL-запроса
-sql_select_step = PostgresOperator(
+sql_select_step = SQLExecuteQueryOperator(
     task_id="sql_select_step",
-    sql="SELECT * FROM source_data.отрасли",
-    postgres_conn_id='1111',
+    sql='''SELECT * FROM source_data.отрасли''',
+    conn_id='source',
     dag=dag
 )
 
 # Задача DummyOperator, которая служит конечной точкой
-end_step = DummyOperator(task_id="end_step",dag=dag)
+end_step = EmptyOperator(task_id="end_step", dag=dag)
 
 # Определение порядка выполнения задач
 start_step >> sql_select_step >> end_step
